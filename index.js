94e0c41edc058814487a64f6d195e01bdf4b8e31
Wellington Cua <wjcua528@gmail.com>
2021-11-23 21:40:27 -0600

    correccion de errores en archivos

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const paragraphs = document.getElementsByTagName("p");
 console.log("parrafos en el documento: ", paragraphs.length);
 
-if (paragraph.length > 0) {
+if (paragraphs.length > 0) {
     const paragraph = paragraphs[0];
     paragraph.innerText = "Bienvenidos al bootcamp"; //cambio de texto elemento p
+}
+
+if (paragraphs.length > 1) {
+    const paragraph = paragraphs[1];
+    paragraph.innerText = "parrafos en el documento: " + paragraphs.length
 }
