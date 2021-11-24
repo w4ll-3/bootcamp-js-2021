1b027f77bd98e07142b36a39e39757c450a097b6
Wellington Cua <wjcua528@gmail.com>
2021-11-23 21:15:14 -0600

    cambio de texto y let a const

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let paragraphs = document.getElementsByTagName("p");
-console.log(paragraphs.length);
+const paragraphs = document.getElementsByTagName("p");
+console.log("parrafos en el documento: ", paragraphs.length);
 
 if (paragraph.length > 0) {
-    let paragraph = paragraphs[0];
+    const paragraph = paragraphs[0];
     paragraph.innerText = "Bienvenidos al bootcamp"; //cambio de texto elemento p
 }
